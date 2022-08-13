1b3a4d44b174d23b26d7487223ddff4e43cc8b3c
Andrey Aborkin <andrey270304@gmail.com>
2022-08-13 16:28:44 +0300

    added dz3

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿Console.Clear();
+// Задача № 19 Определение является ли пятизначное число полиндромом
+// void culcPolindrom(int number)
+// {
+//     if(number / 10000 == number % 10 && number / 1000  % 10 == number % 100 /10)
+//     {
+//         Console.WriteLine($"{number} является полиндоромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} не является полиндромом");
+//     }
+// }
+
+// Console.WriteLine("Введите пятизначное число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num < 100000 && num > 9999)
+//     {
+//     culcPolindrom(num);
+//     }
+// else
+// {
+// Console.WriteLine($"{num} не является пятизначным числом");
+// }
+
+//Задача № 21 вычисление отрезка по 3 координатам
+// void length(int Ax, int Ay, int Az, int Bx, int By, int Bz)
+// {
+//     double l = Math.Sqrt(Math.Pow(Bx-Ax, 2) + Math.Pow(By-Ay, 2) + Math.Pow(Bz-Az, 2));
+//     Console.WriteLine($"Длина отрезка равна {l:f2}");
+// }
+// Console.WriteLine("Введите координаты первой точки ");
+// int Ax = Convert.ToInt32(Console.ReadLine());
+// int Ay = Convert.ToInt32(Console.ReadLine());
+// int Az = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты второй точки ");
+// int Bx = Convert.ToInt32(Console.ReadLine());
+// int By = Convert.ToInt32(Console.ReadLine());
+// int Bz = Convert.ToInt32(Console.ReadLine());
+// length(Ax, Ay, Az, Bx, By, Bz);
+
+//Задача № 23 Таблица кубов чисел от 1 до n
+// void kubNumbers(int number)
+// {
+//     int n = 1;
+//     while (n <= number)
+//     {
+//         double kubNum = Math.Pow(n, 3);
+//         Console.Write($"{kubNum}, ");
+//         n++;
+//     }
+// }
+        
+// Console.WriteLine("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// kubNumbers(num);
+
